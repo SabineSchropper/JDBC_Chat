1f0211bf703d313e6555f541c21aa23ce4b80572
SabineSchropper <sabine.schropper@gmail.com>
2020-04-10 16:01:33 +0200

    tried to improve chat

--- src/com/company/Chat.java
+++ src/com/company/Chat.java
@@ -1,5 +1,21 @@
 package com.company;
 
-public class Chat {
+import java.util.ArrayList;
 
+public class Chat {
+    ArrayList<User> users = new ArrayList<User>();
+
+    public void addUser(User user){
+        users.add(user);
+    }
+    public boolean checkUser(int userId){
+        boolean isInArrayList = false;
+        for(User user: users){
+            if (user.userId == userId){
+                isInArrayList = true;
+            }
+        }
+        return isInArrayList;
+    }
 }
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,10 +11,12 @@ import java.util.TimerTask;
 public class Main {
 
     public static void main(String[] args) {
+
+        Timer timer = new Timer();
+
+        Chat chat = new Chat();
         Scanner scan = new Scanner(System.in);
         Scanner numberScanner = new Scanner(System.in);
-        Timer timer = new Timer();
-        String url = "jdbc:mysql://localhost:3306/codingcampus?user=root";
 
         System.out.println("Gib deine User_id ein:");
         int userId = numberScanner.nextInt();
@@ -22,55 +24,60 @@ public class Main {
         System.out.println("Gib deinen Namen ein:");
         String name = scan.nextLine();
 
-        User user = new User(userId,name);
+        boolean isUserInArrayList = chat.checkUser(userId);
+        if (!isUserInArrayList) {
+            User user = new User(userId, name);
+            chat.addUser(user);
+        }
 
-
-        
         System.out.println("Gib deine Nachricht ein:");
-        System.out.println("Du kannst den Chat mit x beenden.");
-
 
         boolean isChatting = true;
-
         while (isChatting) {
-            try {
-                Connection connection = DriverManager.getConnection(url);
-                Statement statement = connection.createStatement();
-                String sql = "SELECT max(id) FROM chat";
-                ResultSet rs1 = statement.executeQuery(sql);
-                rs1.next();
-                int number = rs1.getInt(1);
-                //Select and print the second last note
-                sql = "SELECT * FROM chat WHERE id = "+(number-1)+"";
-                ResultSet rs = statement.executeQuery(sql);
-                while (rs.next()) {
-                    String chat = rs.getString("note");
-                    System.out.println(chat);
-                }
-            } catch (SQLException ex) {
-                ex.fillInStackTrace();
-            }
-            //timer.schedule(new TimerTask() {
-            //public void run() {
+
             String messageFromUser = scan.nextLine();
             if (messageFromUser.equalsIgnoreCase("x")) {
                 isChatting = false;
                 continue;
             }
             try {
-                url = "jdbc:mysql://localhost:3306/codingcampus?user=root";
+                String url = "jdbc:mysql://localhost:3306/codingcampus?user=root";
                 Connection connection = DriverManager.getConnection(url);
                 Statement statement = connection.createStatement();
-                String sql = "INSERT INTO chat (note) VALUES ('" + messageFromUser + "')";
+                String sql = "INSERT INTO `chat`(`user_id`, `note`, `user_name`)" +
+                        " VALUES (" + userId + ",'" + messageFromUser + "','" + name + "')";
                 statement.executeUpdate(sql);
             } catch (SQLException ex) {
                 ex.fillInStackTrace();
             }
 
-            //}, 5000, 5000);
 
+            timer.schedule(new TimerTask() {
+                public void run() {
+                    try {
+                        String url = "jdbc:mysql://localhost:3306/codingcampus?user=root";
+                        Connection connection = DriverManager.getConnection(url);
+                        Statement statement = connection.createStatement();
+                        String sql = "SELECT id FROM `chat` WHERE user_id = " + userId + " ORDER BY id DESC LIMIT 1";
+                        ResultSet rs1 = statement.executeQuery(sql);
+                        rs1.next();
+                        int number = rs1.getInt(1);
+                        int idOfLastMessage = 0;
+                        //Select and print the notes from the other users with an id greater than the id of my entry
+                        sql = "SELECT * FROM chat WHERE user_id != " + userId + " AND id > " + number + " AND id > " + idOfLastMessage + "";
+                        ResultSet rs = statement.executeQuery(sql);
+
+                        while (rs.next()) {
+                            String chatString = rs.getString("note");
+                            idOfLastMessage = rs.getInt("id");
+                            System.out.println(chatString);
+                        }
+                    } catch (SQLException ex) {
+                        ex.fillInStackTrace();
+                    }
+                }
+            }, 3000, 3000);
         }
     }
 }
-//}
 
