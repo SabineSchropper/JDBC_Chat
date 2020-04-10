474d1c5a6696952f1fb7a741ea73f586dd4e0779
SabineSchropper <sabine.schropper@gmail.com>
2020-04-10 20:49:20 +0200

    chat works

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,17 +12,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Timer timer = new Timer();
-
         Chat chat = new Chat();
         Scanner scan = new Scanner(System.in);
         Scanner numberScanner = new Scanner(System.in);
+
+        boolean isChatting = true;
 
         System.out.println("Gib deine User_id ein:");
         int userId = numberScanner.nextInt();
 
         System.out.println("Gib deinen Namen ein:");
         String name = scan.nextLine();
+
+        Timer timer = new Timer();
+        MyTimerTask task = new MyTimerTask(userId);
+        timer.schedule(task, 0, 1500);
 
         boolean isUserInArrayList = chat.checkUser(userId);
         if (!isUserInArrayList) {
@@ -32,7 +36,6 @@ public class Main {
 
         System.out.println("Gib deine Nachricht ein:");
 
-        boolean isChatting = true;
         while (isChatting) {
 
             String messageFromUser = scan.nextLine();
@@ -50,34 +53,8 @@ public class Main {
             } catch (SQLException ex) {
                 ex.fillInStackTrace();
             }
-
-
-            timer.schedule(new TimerTask() {
-                public void run() {
-                    try {
-                        String url = "jdbc:mysql://localhost:3306/codingcampus?user=root";
-                        Connection connection = DriverManager.getConnection(url);
-                        Statement statement = connection.createStatement();
-                        String sql = "SELECT id FROM `chat` WHERE user_id = " + userId + " ORDER BY id DESC LIMIT 1";
-                        ResultSet rs1 = statement.executeQuery(sql);
-                        rs1.next();
-                        int number = rs1.getInt(1);
-                        int idOfLastMessage = 0;
-                        //Select and print the notes from the other users with an id greater than the id of my entry
-                        sql = "SELECT * FROM chat WHERE user_id != " + userId + " AND id > " + number + " AND id > " + idOfLastMessage + "";
-                        ResultSet rs = statement.executeQuery(sql);
-
-                        while (rs.next()) {
-                            String chatString = rs.getString("note");
-                            idOfLastMessage = rs.getInt("id");
-                            System.out.println(chatString);
-                        }
-                    } catch (SQLException ex) {
-                        ex.fillInStackTrace();
-                    }
-                }
-            }, 3000, 3000);
         }
     }
 }
+
 
---
+++ src/com/company/MyTimerTask.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.sql.*;
+import java.util.TimerTask;
+
+public class MyTimerTask extends TimerTask {
+    int userId;
+    int id;
+    int idBefore;
+
+    public MyTimerTask(int userId){
+        this.userId = userId;
+    }
+    @Override
+    public void run() {
+
+        try {
+
+            String url = "jdbc:mysql://localhost:3306/codingcampus?user=root";
+            Connection connection = DriverManager.getConnection(url);
+            Statement statement = connection.createStatement();
+            String sql = "SELECT max(id) FROM `chat`";
+            ResultSet rs = statement.executeQuery(sql);
+            rs.next();
+            id = rs.getInt(1);
+            if(idBefore != id) {
+                String sql1 = "SELECT * FROM chat WHERE id = " + id + " AND user_id != " + userId + "";
+                ResultSet rs1 = statement.executeQuery(sql1);
+                while (rs1.next()) {
+                    String note = rs1.getString("note");
+                    System.out.println(note);
+                }
+            }
+        } catch (SQLException ex) {
+            ex.fillInStackTrace();
+        }
+        idBefore = id;
+    }
+}
